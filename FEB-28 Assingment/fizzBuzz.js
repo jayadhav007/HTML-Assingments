ba119468b9d18365a6d5953a18d727b8eb3f79ed
jayadhav007 <96857887+jayadhav007@users.noreply.github.com>
2022-03-01 17:14:43 +0530

    Add files via upload

---
+++ FEB-28 Assingment/fizzBuzz.js
@@ -0,0 +1,29 @@
+function EnterNumber(){
+  var n = prompt("enter number")
+  var n = parseInt(n);
+  if(isNaN(n)){
+    alert("Enter a correct  number");
+
+    
+
+  }
+  else
+  for (var i = 1; i <= n; i++) {
+    if (i % 15 == 0) {
+     document.write("FizzBuzz");
+    }
+    else if (i % 3 == 0){
+     document.write("Fizz");
+  }
+    else if (i % 5 == 0) {
+    document.write("Buzz");
+    }
+    else{ document.write(i);
+    }
+}
+  
+  
+  
+  
+  
+}
